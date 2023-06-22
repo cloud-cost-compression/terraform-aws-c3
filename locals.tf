41ba41f14cec972f7fcb8a8a266aa247a8f81def
Samuel Hopko <samuel.hopko@gmail.com>
2023-06-22 21:51:02 +0200

    Update of provisioning scripts

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
     Project = "c3"
   }
 
-  k8s_service_account_name = "evl-job-sa"
+  k8s_service_account_name = "c3"
   k8s_namespace_name       = "c3"
 
   c3_admin_aws_account_id = "397307921369"
